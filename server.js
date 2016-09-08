f818e0dd63234c4e4fa447adb102b67f6f2f51d4
Srinivas Thungathurti <89589.thungathurti@students.itu.edu>
2016-09-08 16:48:37 -0700

    node server config changed to get dynamic port from heroku

--- server.js
+++ server.js
@@ -872,6 +872,8 @@ app.all('/*', function(req, res, next) {
 	});
 });
 
-app.listen(port,function() {
+app.listen(process.env.PORT || 1337,function() {
+	console.log('http://127.0.0.1:' + port + '/');
+});
 	console.log('http://127.0.0.1:' + port + '/');
 });
