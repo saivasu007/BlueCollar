74362d215943c4ddaac95c830422498c3e751196
Srinivas Thungathurti <89589.thungathurti@students.itu.edu>
2016-09-12 22:34:10 -0700

    Updated oAuth settings

--- oauth.js
+++ oauth.js
@@ -2,17 +2,17 @@ var ids = {
   facebook: {
     clientID: '1598117983823240',
     clientSecret: '62c7b61da6b42ddb39efea8b25462d8c',
-    callbackURL: 'https://itubluecollar.herokuapp.com/auth/facebook/callback'
+    callbackURL: 'www.bluecollarhunt.com/auth/facebook/callback'
   },
   google: {
-    clientID: '8146498752-hdommt7s414bmhlpocl3euaklqsqriel.apps.googleusercontent.com',
-    clientSecret: '78cFdH1dYrKXtRDsyckA1s4Z',
-    returnURL: 'https://itubluecollar.herokuapp.com/auth/google/callback'
+    clientID: '1003631882203-h39iko7mudtca2h2mmqjpi1ekpac8q6s.apps.googleusercontent.com',
+    clientSecret: 'KWuXkAMVlf4aXuuXrYTK8cC0',
+    returnURL: 'www.bluecollarhunt.com/auth/google/callback'
   },
    linkedin: {
-    consumerKey: '7578yq7jpw4ui9',
-    consumerSecret: '5fHsmNyBb3bEYWlV',
-    callbackURL: 'https://itubluecollar.herokuapp.com/auth/linkedin/callback',
+    consumerKey: '7515ix697gi6oz',
+    consumerSecret: 'zPbAKreoQuQg3U3H',
+    callbackURL: 'www.bluecollarhunt.com/auth/linkedin/callback',
     scope: ['r_emailaddress', 'r_basicprofile']
   }
 };
