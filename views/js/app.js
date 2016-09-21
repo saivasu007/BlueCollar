96e4dc05c4052d5676ef856b4bd195c3f9929d4a
Srinivas Thungathurti <89589.thungathurti@students.itu.edu>
2016-09-20 22:48:30 -0700

    User Profile page added

--- views/js/app.js
+++ views/js/app.js
@@ -718,6 +718,7 @@ app.controller('homeCtrl', function ($q, $scope, $rootScope, $http, $location, $
 	
 
 	$scope.logout = function () {
+		alert("Logout of application");
 		$http.post('/logout',$rootScope.user).success(function () {
 			$location.url('/');
 			$rootScope.currentUser = undefined;
@@ -872,269 +873,20 @@ app.controller('contactCtrl', function ($q, $scope, $rootScope, $http, $location
 
 
 //Updated by Srinivas Thungathurti for ASQ Upgrade 2.0
-app.controller('profileCtrl', function ($q, $scope, $rootScope, $http, $location) {	
-	$scope.firstName = $rootScope.currentUser.firstName;
-	$scope.states = [
-	                 {
-	                     "name": "Alabama",
-	                     "abbreviation": "AL"
-	                 },
-	                 {
-	                     "name": "Alaska",
-	                     "abbreviation": "AK"
-	                 },
-	                 {
-	                     "name": "Arizona",
-	                     "abbreviation": "AZ"
-	                 },
-	                 {
-	                     "name": "Arkansas",
-	                     "abbreviation": "AR"
-	                 },
-	                 {
-	                     "name": "California",
-	                     "abbreviation": "CA"
-	                 },
-	                 {
-	                     "name": "Colorado",
-	                     "abbreviation": "CO"
-	                 },
-	                 {
-	                     "name": "Connecticut",
-	                     "abbreviation": "CT"
-	                 },
-	                 {
-	                     "name": "Delaware",
-	                     "abbreviation": "DE"
-	                 },
-	                 {
-	                     "name": "District Of Columbia",
-	                     "abbreviation": "DC"
-	                 },
-	                 {
-	                     "name": "Florida",
-	                     "abbreviation": "FL"
-	                 },
-	                 {
-	                     "name": "Georgia",
-	                     "abbreviation": "GA"
-	                 },
-	                 {
-	                     "name": "Hawaii",
-	                     "abbreviation": "HI"
-	                 },
-	                 {
-	                     "name": "Idaho",
-	                     "abbreviation": "ID"
-	                 },
-	                 {
-	                     "name": "Illinois",
-	                     "abbreviation": "IL"
-	                 },
-	                 {
-	                     "name": "Indiana",
-	                     "abbreviation": "IN"
-	                 },
-	                 {
-	                     "name": "Iowa",
-	                     "abbreviation": "IA"
-	                 },
-	                 {
-	                     "name": "Kansas",
-	                     "abbreviation": "KS"
-	                 },
-	                 {
-	                     "name": "Kentucky",
-	                     "abbreviation": "KY"
-	                 },
-	                 {
-	                     "name": "Louisiana",
-	                     "abbreviation": "LA"
-	                 },
-	                 {
-	                     "name": "Maine",
-	                     "abbreviation": "ME"
-	                 },
-	                 {
-	                     "name": "Maryland",
-	                     "abbreviation": "MD"
-	                 },
-	                 {
-	                     "name": "Massachusetts",
-	                     "abbreviation": "MA"
-	                 },
-	                 {
-	                     "name": "Michigan",
-	                     "abbreviation": "MI"
-	                 },
-	                 {
-	                     "name": "Minnesota",
-	                     "abbreviation": "MN"
-	                 },
-	                 {
-	                     "name": "Mississippi",
-	                     "abbreviation": "MS"
-	                 },
-	                 {
-	                     "name": "Missouri",
-	                     "abbreviation": "MO"
-	                 },
-	                 {
-	                     "name": "Montana",
-	                     "abbreviation": "MT"
-	                 },
-	                 {
-	                     "name": "Nebraska",
-	                     "abbreviation": "NE"
-	                 },
-	                 {
-	                     "name": "Nevada",
-	                     "abbreviation": "NV"
-	                 },
-	                 {
-	                     "name": "New Hampshire",
-	                     "abbreviation": "NH"
-	                 },
-	                 {
-	                     "name": "New Jersey",
-	                     "abbreviation": "NJ"
-	                 },
-	                 {
-	                     "name": "New Mexico",
-	                     "abbreviation": "NM"
-	                 },
-	                 {
-	                     "name": "New York",
-	                     "abbreviation": "NY"
-	                 },
-	                 {
-	                     "name": "North Carolina",
-	                     "abbreviation": "NC"
-	                 },
-	                 {
-	                     "name": "North Dakota",
-	                     "abbreviation": "ND"
-	                 },
-	                 {
-	                     "name": "Ohio",
-	                     "abbreviation": "OH"
-	                 },
-	                 {
-	                     "name": "Oklahoma",
-	                     "abbreviation": "OK"
-	                 },
-	                 {
-	                     "name": "Oregon",
-	                     "abbreviation": "OR"
-	                 },
-	                 {
-	                     "name": "Pennsylvania",
-	                     "abbreviation": "PA"
-	                 },
-	                 {
-	                     "name": "Rhode Island",
-	                     "abbreviation": "RI"
-	                 },
-	                 {
-	                     "name": "South Carolina",
-	                     "abbreviation": "SC"
-	                 },
-	                 {
-	                     "name": "South Dakota",
-	                     "abbreviation": "SD"
-	                 },
-	                 {
-	                     "name": "Tennessee",
-	                     "abbreviation": "TN"
-	                 },
-	                 {
-	                     "name": "Texas",
-	                     "abbreviation": "TX"
-	                 },
-	                 {
-	                     "name": "Utah",
-	                     "abbreviation": "UT"
-	                 },
-	                 {
-	                     "name": "Vermont",
-	                     "abbreviation": "VT"
-	                 },
-	                 {
-	                     "name": "Virginia",
-	                     "abbreviation": "VA"
-	                 },
-	                 {
-	                     "name": "Washington",
-	                     "abbreviation": "WA"
-	                 },
-	                 {
-	                     "name": "West Virginia",
-	                     "abbreviation": "WV"
-	                 },
-	                 {
-	                     "name": "Wisconsin",
-	                     "abbreviation": "WI"
-	                 },
-	                 {
-	                     "name": "Wyoming",
-	                     "abbreviation": "WY"
-	                 }
-	               ];
-	
-	$scope.selectedState = $rootScope.currentUser.state;
+app.controller('profileCtrl', function ($q, $scope, $rootScope, $http, $location) {
 	$scope.logout = function () {
 		$http.post('/logout',$rootScope.user).success(function () {
 			$location.url('/');
 			$rootScope.currentUser = undefined;
-			$rootScope.user = undefined;
-		})
-	};
-	//Updated Password validation as the current behavior is asking user to fill in the New/Repeated Password while updating profile.
-	$scope.save = function (currentUser) {
-		
-	   if ($scope.currentUser.firstName == "" || $scope.currentUser.lastName == "" || $scope.currentUser.address1 == "" || $scope.currentUser.city == "" || $scope.selectedState == "" || $scope.currentUser.zipcode == "" || $scope.currentUser.birthDate == "") {
-			alert("Please fill in all the blanks!");
-		}
-		else {
-			var postData = {
-				email: currentUser.email,
-				firstName: currentUser.firstName,
-				lastName: currentUser.lastName,
-				address1: currentUser.address1,
-				address2: currentUser.address2,
-				city: currentUser.city,
-				state: $scope.selectedState,
-				zipcode: currentUser.zipcode,
-				birthDate:moment(currentUser.birthDate).format("MM/DD/YYYY")
-			};
-		}
-		$http.post('/updateProfile', postData).success(function (response) {
-			if (response == 'success'){
-				$scope.firstName = postData.firstName;
-				console.log("Update "+response);
-				alert('Success!');
-				$location.url('/home');
-			} else if (response == 'error') {
-				alert('error')
-			}
+			$scope.user = undefined;
 		})
 	};
 
-	// Delete the below code for ASQ Upgrade 2.0.
-	/*
-	$scope.wrong = false;
-	$scope.errorClass = "";
-	$scope.checkPasswd = function () {
-		if ($scope.currentUser.passwd1 !== $scope.currentUser.passwd2) {
-			$scope.wrong = true;
-			$scope.errorClass = "has-error";
+	$scope.pressEnter = function (e,user) {
+		if (e.keyCode == 13){
+			$scope.admin(user);
 		}
-		else {
-			$scope.wrong = false;
-			$scope.errorClass = "";
-		}
-	}; */
-
+	};
 });
 
 //Added by Srinivas Thungathurti for ASQ Upgrade 2.0.New screen Change Password added to application.
