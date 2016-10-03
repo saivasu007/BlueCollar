11d2a7aa25ecb272f337c74ee4cbca4d4cf01cc3
Srinivas Thungathurti <89589.thungathurti@students.itu.edu>
2016-10-03 11:30:45 -0700

    fixed file by removing unwanted links

--- views/js/app.js
+++ views/js/app.js
@@ -3,7 +3,7 @@
  * @author : Srinivas Thungathurti
  * @description : Modified for ASQ Upgrade 2.0 changes for Sprint 1 (Registration and Login requirements).
  */
-var app = angular.module('blueCollarApp', ['ngRoute', 'highcharts-ng','toggle-switch','timer','ui.bootstrap','ngAutocomplete','angularFileUpload','reader','ngImageInputWithPreview','ngFlash']);
+var app = angular.module('blueCollarApp', ['ngRoute', 'highcharts-ng','toggle-switch','timer','ui.bootstrap','ngAutocomplete','angularFileUpload','ngImageInputWithPreview','ngFlash']);
 
 
 //Added by Srinivas Thungathurti for ASQ Upgrade2.0 for adding calendar fields on register/profile/updateUserInfo screens.
